811bb5306541f6957371ec4effacb8ebaa94478c
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-10-20 19:47:49 -0400

    added mongoose schemas for layers, properties, tilemaps, and tilesets

--- server/database/mongoose/schemas/user.ts
+++ server/database/mongoose/schemas/user.ts
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const ObjectId = Schema.Types.ObjectId
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
 /**
  * Data model for storing personal user data
@@ -8,19 +8,19 @@ const ObjectId = Schema.Types.ObjectId
  * @
  */
 const UserSchema = new Schema({
-    firstName: { type: String, required: true},
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    username: { type: String, required: true },
-    passwordHash: { type: String, required: true },
-    verifyKey: { type: String, required: true },
-    isVerified: { type: Boolean, required: true },
-    favoriteTileMaps: { type: ObjectId, required: true },
-    favoriteTileSets: { type: ObjectId, required: true },
-    joinedContests: { type: [ObjectId], required: true},
-    joinedCommunities: { type: [ObjectId], required: true},
-    friends: { type: [ObjectId], required: true},
-    imageURL: { type: String, required: true }
-})
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  verifyKey: { type: String, required: true },
+  isVerified: { type: Boolean, required: true },
+  favoriteTileMaps: { type: ObjectId, required: true },
+  favoriteTileSets: { type: ObjectId, required: true },
+  joinedContests: { type: [ObjectId], required: true },
+  joinedCommunities: { type: [ObjectId], required: true },
+  friends: { type: [ObjectId], required: true },
+  imageURL: { type: String, required: true },
+});
 
-module.exports = mongoose.model('User', UserSchema)
+exports = mongoose.model("User", UserSchema);
