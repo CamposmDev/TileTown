961a291b4f77532520c7757ff4ee8b0f71e90dd1
Tuyen Vo <thituyen1996@gmail.com>
2022-10-20 17:43:51 -0400

    Update Community Forum Post and Implement Forum Post Schema

--- server/database/mongoose/schemas/communityForumPost.ts
+++ server/database/mongoose/schemas/communityForumPost.ts
@@ -10,13 +10,11 @@ const CommunityForumPostSchema = new Schema({
     author: { type: String, require: true},
     title: { type: String, require: true},
     body: { type: String, require: true},
-    tags: {type: [String], require: true},
-    likes: {type: [ObjectId], require: true},
-    dislikes: {type: [ObjectId], require: true},
-    isPublished: {type: Boolean, require: true}
-
-
-
+    communityId: {type: String, require: true},
+    tags: { type: [String], require: true},
+    likes: { type: [ObjectId], require: true},
+    dislikes: { type: [ObjectId], require: true},
+    isPublished: { type: Boolean, require: true}
 
 })
 export {CommunityForumPostSchema}
---
+++ server/database/mongoose/schemas/forumPostSchema.ts
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.Object
+
+/**
+ * @author Tuyen Vo
+ */
+
+const ForumPostSchema = new Schema({
+    author: { type: String, require: true},
+    title: { type: String, require: true},
+    body: { type: String, require: true},
+    tags: {type: [String], require: true},
+    likes: {type: [ObjectId], require: true},
+    dislikes: {type: [ObjectId], require: true},
+    isPublished: {type: Boolean, require: true}
+
+
+
+
+})
+export {ForumPostSchema}
