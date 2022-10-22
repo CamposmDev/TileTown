f2c417e557edecc3abc22fcbf3117152ff302401
Michael Campos <camposm.dev@gmail.com>
2022-10-22 04:42:30 -0400

    Implemented function getForumPost
    Allows user to retrieve forum post data

--- server/database/mongoose/managers/MongooseForumDBM.ts
+++ server/database/mongoose/managers/MongooseForumDBM.ts
@@ -1,11 +1,26 @@
 import { ForumPost } from "../../../types";
 import ForumDBM from "../../interface/managers/ForumDBM";
+import ForumPostSchema from '../../mongoose/schemas/forumPost'
 
 export default class MongooseForumDBM implements ForumDBM {
 
     async getForumPost(forumPostId: string): Promise<ForumPost | null> {
-        throw new Error("Method not implemented.");
+        let forumPost: any = await ForumPostSchema.findById(forumPostId)
+        if (forumPost !== null) {
+            return {
+                id: forumPost._id.toString(),
+                author: forumPost.author,
+                title: forumPost.title,
+                body: forumPost.body,
+                tags: forumPost.tags,
+                likes: forumPost.likes,
+                dislikes: forumPost.dislikes,
+                isPublished: forumPost.isPublished
+            }
+        }
+        return null
     }
+
     async createForumPost(forumPost: Partial<ForumPost>): Promise<ForumPost | null> {
         throw new Error("Method not implemented.");
     }
