acd1066971b3c2432ae344ba9773eea860bffe14
Michael Campos <camposm.dev@gmail.com>
2022-10-23 14:13:12 -0400

    Implemented function getForumPostById in ForumController

--- server/express/controllers/ExpressForumController.ts
+++ server/express/controllers/ExpressForumController.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from 'express';
 import { db } from '../../database';
+import { ForumPost } from '../../types';
 
 export default class ForumController {
 
     public async getForumPostById(req: Request, res: Response): Promise<void> {
-        res.status(200).json({message: "Getting a forum post by id!"});
+        /** If there ins't an id in the url params return 400 */
+        if (!req || !req.params || !req.params.id) {
+            res.status(400).json({message: 'Bad Request'})
+            return
+        }
+
+        /** If there isn't a forum post in the database with the id return 404 */
+        let id = req.params.id
+        let forumPost: ForumPost | null = await db.forums.getForumPost(id)
+        if (forumPost === null) {
+            res.status(404).json({message: 'Forum post does not exist'})
+            return
+        }
+
+        /** Otherwise return all the data about the forum post to the client */
+        res.status(200).json({forumPost: forumPost});
+        return
     }
 
     public async createForumPost(req: Request, res: Response): Promise<void> {
