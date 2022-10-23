23a89d1df45602548b0085c69432f7f72a8441b0
Tuyen Vo <thituyen1996@gmail.com>
2022-10-22 21:49:57 -0400

    Implement toggleLike Function in MongooseTilesetDBM

--- server/database/mongoose/managers/MongooseTilesetDBM.ts
+++ server/database/mongoose/managers/MongooseTilesetDBM.ts
@@ -54,8 +54,40 @@ export default class MongooseTilesetDBM implements TilesetDBM {
         return null
     }
     async toggleLike(userId: string, socialId: string): Promise<TilesetSocialStatistics | null> {
-        throw new Error("Method not implemented.");
-    }
+        let user = await UserSchema.findById(userId)
+    let social: any = await TilesetSocialStatisticsSchema.findById(socialId)
+    if ((user !== null) && (social !== null)) {
+      let id = user._id.toString()
+      let likes = social.likes
+      let dislikes = social.dislikes
+      if (!dislikes.includes(id)) {
+        if (likes.includes(id)) {
+          let i = likes.indexOf(id, 0)
+          likes.splice(i, 1)
+        } else {
+          likes.push(id)
+        }
+        await social.save()
+        return {
+          tileset: social.tileset,
+          name: social.name,
+          owner: social.owner,
+          ownerName: social.ownerName,
+          tags: social.tags,
+          description: social.description,
+          communities: social.communities,
+          likes: social.likes,
+          dislikes: social.dislikes,
+          views: social.views,
+          permissions: social.permissions,
+          comments: social.comments,
+          publishDate: social.publishDate,
+          imageURL: social.imageURL
+        }
+      }
+    }  
+    return null
+  }
     async toggleDislike(userId: string, socialId: string): Promise<TilesetSocialStatistics | null> {
         throw new Error("Method not implemented.");
     }
