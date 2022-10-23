e0b948dce0a8a33369d59091b009695e2d566e52
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-10-23 14:49:04 -0400

    added in create tilemap in controller, renamed tilemappairids to tilemappartials, added is library to check types

--- server/express/controllers/ExpressTilemapController.ts
+++ server/express/controllers/ExpressTilemapController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { db } from "../../database";
+import { Tilemap } from "../../types";
+import { is } from "typescript-is";
 
 export default class TilemapController {
   public async getTilemapById(req: Request, res: Response): Promise<void> {
@@ -17,12 +19,54 @@ export default class TilemapController {
     res.status(200).json({ message: "Getting tilemap pairs!" });
   }
 
-  public async createTilemap(req: Request, res: Response): Promise<void> {
-    const body = req.body;
-    if (!body) {
+  public async createTilemap(req: Request, res: Response): Promise<Response> {
+    //check to see if a request body was sent
+    if (!req.body) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
     }
 
-    res.status(200).json({ message: "Creating tilemap!" });
+    let userId: string = req.userId;
+
+    //check to see if a user id was provided and if it was formatted as a string
+    if (!userId || !is<string>(userId)) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    let tilemap: Partial<Tilemap> = req.body.userId;
+
+    //check to see if a tilemap partial was provided and if it was formatted properly
+    if (!tilemap || !is<Partial<Tilemap>>(tilemap)) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    const response: Partial<Tilemap> | string = await db.tilemaps.createTilemap(
+      userId,
+      tilemap
+    );
+
+    //check for error messages
+    if (is<string>(response)) {
+      return res.status(400).json({
+        errorMessage: response,
+      });
+    }
+
+    //make sure response is at in the format of a tilemap partial
+    if (!is<Partial<Tilemap>>(response)) {
+      return res.status(400).json({
+        errorMessage: "unable to create new tilemap",
+      });
+    }
+
+    return res
+      .status(201)
+      .json({ message: "Creating tilemap!", response: response });
   }
 
   public async deleteTilemapById(req: Request, res: Response): Promise<void> {
--- server/express/routers/TilemapRouter.ts
+++ server/express/routers/TilemapRouter.ts
@@ -3,19 +3,18 @@ import { TilemapController } from "../controllers";
 
 const router: Router = Router();
 
-router.get('/:id', TilemapController.getTilemapById);
-router.get('/social/:id', TilemapController.getTilemapSocialStatsById);
-router.get('/pairs/:search/:sort', TilemapController.getTiemapPairs);
+router.get("/:id", TilemapController.getTilemapById);
+router.get("/social/:id", TilemapController.getTilemapSocialStatsById);
+router.get("/pairs/:search/:sort", TilemapController.getTilemapPartials);
 
-router.post('/', TilemapController.createTilemap);
-router.post('/comment/:id', TilemapController.commentTilemapById);
+router.post("/", TilemapController.createTilemap);
+router.post("/comment/:id", TilemapController.commentTilemapById);
 
-router.put('/:id', TilemapController.updateTilemapById);
-router.put('/like/:id', TilemapController.likeTilemapById);
-router.put('/dislike/:id', TilemapController.dislikeTilemapById);
-router.put('/view/:id', TilemapController.viewTilemapById);
+router.put("/:id", TilemapController.updateTilemapById);
+router.put("/like/:id", TilemapController.likeTilemapById);
+router.put("/dislike/:id", TilemapController.dislikeTilemapById);
+router.put("/view/:id", TilemapController.viewTilemapById);
 
-router.delete('/:id', TilemapController.deleteTilemapById);
-
+router.delete("/:id", TilemapController.deleteTilemapById);
 
 export default router;
