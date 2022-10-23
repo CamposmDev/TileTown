0a484190b75515af3fc3b4acf78ee94292409819
Tuyen Vo <thituyen1996@gmail.com>
2022-10-23 18:15:22 -0400

    Implement getContestById Function in ExpressContestController

--- server/express/controllers/ExpressContestController.ts
+++ server/express/controllers/ExpressContestController.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express';
 import { db } from "../../database";
+import { Contest } from '../../types';
 
 export default class ContestController {
 
     public async getContestById(req: Request, res: Response): Promise<void> {
-        res.status(200).json({message: "Getting a contest by id!"});
+         /** If there isn't an id in the url params return 400 - Bad Request */
+         if (!req || !req.params || !req.params.id) {
+            res.status(400).json({message: "Bad Request!"})
+            return
+        }
+        /** If there isn't a contest in the database with the id return 404 - Not Found */
+        let id: string = req.params.id
+        let contest: Contest | null = await db.contests.getContest(id)
+        if (contest === null) {
+
+            res.status(404).json({message: "Contest does not exist"})
+            return
+        }
+
+        /** Otherwise return all the data about the contest back to the client */
+        res.status(200).json({contest: contest})
+        return
     }
+
 
     public async createContest(req: Request, res: Response): Promise<void> {
         res.status(200).json({message: "Creating a contest!"});
