a1329e3184641114dd78c67690155c141aff712e
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-10-23 15:28:43 -0400

    added in delete tile map and update tile map

--- server/express/controllers/ExpressTilemapController.ts
+++ server/express/controllers/ExpressTilemapController.ts
@@ -39,7 +39,7 @@ export default class TilemapController {
     }
 
     return res
-      .status(201)
+      .status(200)
       .json({ message: "Getting tilemap!", response: response });
   }
 
@@ -160,12 +160,97 @@ export default class TilemapController {
       .json({ message: "Creating tilemap!", response: response });
   }
 
-  public async deleteTilemapById(req: Request, res: Response): Promise<void> {
-    res.status(200).json({ message: "Deleting tilemap by id!" });
+  public async deleteTilemapById(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    //check to see if a request body was sent
+    if (!req.body) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    const tileMapId: string = req.body.tileMapId;
+
+    //check to see if a tilemap id was provided and if it was formatted as a string
+    if (!tileMapId || !is<string>(tileMapId)) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    const response: Partial<Tilemap> | string =
+      await db.tilemaps.deleteTilemapById(tileMapId);
+
+    //check for error messages
+    if (is<string>(response)) {
+      return res.status(400).json({
+        errorMessage: response,
+      });
+    }
+
+    //make sure response is at in the format of a tilemap partial
+    if (!response || !is<Partial<Tilemap>>(response)) {
+      return res.status(400).json({
+        errorMessage: "unable to delete tilemap",
+      });
+    }
+
+    return res
+      .status(201)
+      .json({ message: "Deleting tilemap!", response: response });
   }
 
-  public async updateTilemapById(req: Request, res: Response): Promise<void> {
-    res.status(200).json({ message: "Updating tilemap by id!" });
+  public async updateTilemapById(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    //check to see if a request body was sent
+    if (!req.body) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    const userId: string = req.userId;
+
+    //check to see if a user id was provided and if it was formatted as a string
+    if (!userId || !is<string>(userId)) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    const tilemap: Partial<Tilemap> = req.body.userId;
+
+    //check to see if a tilemap partial was provided and if it was formatted properly
+    if (!tilemap || !is<Partial<Tilemap>>(tilemap)) {
+      return res.status(400).json({
+        errorMessage: "Improperly formatted request",
+      });
+    }
+
+    const response: Partial<Tilemap> | string =
+      await db.tilemaps.updateTilemapById(userId, tilemap);
+
+    //check for error messages
+    if (is<string>(response)) {
+      return res.status(400).json({
+        errorMessage: response,
+      });
+    }
+
+    //make sure response is at in the format of a tilemap partial
+    if (!response || !is<Partial<Tilemap>>(response)) {
+      return res.status(400).json({
+        errorMessage: "unable to update new tilemap",
+      });
+    }
+
+    return res
+      .status(201)
+      .json({ message: "updating tilemap!", response: response });
   }
 
   public async likeTilemapById(req: Request, res: Response): Promise<void> {
