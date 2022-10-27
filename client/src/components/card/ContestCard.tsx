008bdcc2d1d93185e97316a5c7cf5e4be90ce8b0
Michael Campos <camposm.dev@gmail.com>
2022-10-27 18:35:54 -0400

    Fixed ContestCard margins
    Caused by button wrapper

--- client/src/components/card/ContestCard.tsx
+++ client/src/components/card/ContestCard.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Card, CardContent, Grid, Stack, Typography } from "@mui/material"
+import { Box, Button, Card, CardActionArea, CardContent, Grid, Stack, Typography } from "@mui/material"
+import TouchRipple from "@mui/material/ButtonBase/TouchRipple"
 import { parseDateToStr, calcTimeLeft } from '../../utils/DateUtils'
 
 interface Props {
@@ -18,34 +19,34 @@ const ContestCard = (props: Props) => {
     const timeLeft = calcTimeLeft(props.payload.startDate, props.payload.endDate)
 
     return (
-        <Button sx={{textTransform: 'none', textAlign: 'left'}}>
             <Card>
-                <CardContent>
-                    <Typography>{props.payload.contestName}</Typography>
-                    <Grid container direction='row'>
-                        <Stack direction='column' mr={1}>
-                            <Card sx={{borderRadius: 3, pl: 1, pr: 1, bgcolor: 'secondary.main', color: 'white'}}>
-                                <Typography variant='caption'>{timeLeft}</Typography>
-                            </Card>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography>{props.payload.contestName}</Typography>
+                        <Grid container direction='row'>
+                            <Stack direction='column' mr={1}>
+                                <Card sx={{borderRadius: 3, pl: 1, pr: 1, bgcolor: 'secondary.main', color: 'white'}}>
+                                    <Typography variant='caption'>{timeLeft}</Typography>
+                                </Card>
+                                <Box flexGrow={1}/>
+                                <Typography variant='caption'><b>By</b>:&ensp;{props.payload.ownerName}</Typography>
+                            </Stack>
                             <Box flexGrow={1}/>
-                            <Typography variant='caption'><b>By</b>:&ensp;{props.payload.ownerName}</Typography>
-                        </Stack>
-                        <Box flexGrow={1}/>
-                        <Stack direction='column' spacing={1}>
-                            <Typography variant='caption'>
-                                <b>Theme</b>:&ensp;{props.payload.contestTheme}
-                            </Typography>
-                            <Typography variant='caption'><b>Started:&ensp;</b>
-                                {parseDateToStr(props.payload.startDate)}
-                            </Typography>
-                            <Typography variant='caption'>
-                                <b>{props.payload.numOfParticipates}</b>&ensp;Participates
-                            </Typography>
-                        </Stack>
-                    </Grid>
-                </CardContent>
+                            <Stack direction='column' spacing={1}>
+                                <Typography variant='caption'>
+                                    <b>Theme</b>:&ensp;{props.payload.contestTheme}
+                                </Typography>
+                                <Typography variant='caption'><b>Started:&ensp;</b>
+                                    {parseDateToStr(props.payload.startDate)}
+                                </Typography>
+                                <Typography variant='caption'>
+                                    <b>{props.payload.numOfParticipates}</b>&ensp;Participates
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                    </CardContent>
+                </CardActionArea>
             </Card>
-        </Button>
     )
 }
 
