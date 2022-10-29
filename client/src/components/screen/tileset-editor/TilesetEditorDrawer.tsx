d37e5327eeaa4360a506813ff4563e137504f64d
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-10-29 10:28:35 -0400

    added restrict to tile

--- client/src/components/screen/tileset-editor/TilesetEditorDrawer.tsx
+++ client/src/components/screen/tileset-editor/TilesetEditorDrawer.tsx
@@ -1,73 +1,129 @@
-import { NavigateBefore, Tune } from "@mui/icons-material"
-import { IconButton, Drawer, Grid, Divider, Typography, TextField, Stack, InputAdornment, Checkbox, FormControlLabel } from "@mui/material"
-import { useState } from "react"
+import { NavigateBefore, Tune } from "@mui/icons-material";
+import {
+  IconButton,
+  Drawer,
+  Grid,
+  Divider,
+  Typography,
+  TextField,
+  Stack,
+  InputAdornment,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material";
+import { useState } from "react";
 
 const TilesetEditorDrawer = () => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    const handleMenuOpen = () => setIsDrawerOpen(true)
+  const handleMenuOpen = () => setIsDrawerOpen(true);
 
-    const handleMenuClose = () => setIsDrawerOpen(false)
+  const handleMenuClose = () => setIsDrawerOpen(false);
 
-    const drawer = (
-        <Drawer
-            anchor='left' 
-            open={isDrawerOpen} 
-            onClose={handleMenuClose}
-            PaperProps={{sx: { mt: 15 }}}
-            BackdropProps={{ invisible: true }}
-        >
-            <Stack spacing={1} width='300px' role='presentation' textAlign='start' p={1}>
-                <Grid container alignItems='center'>
-                    <IconButton onClick={handleMenuClose} children={<NavigateBefore/>}/>
-                    <Typography>Properties</Typography>
-                </Grid>
-                <Divider/>
-                <Stack direction='row' alignItems='center' spacing={1}>
-                    <TextField label='Tileset Name' fullWidth size='small' />
-                </Stack>
-                <Grid container alignItems='center' pt={2}>
-                    <Typography>Image Size</Typography>
-                </Grid>
-                <Divider/>
-                <Stack direction='row' alignItems='center' spacing={1}>
-                    <TextField label='Width' fullWidth size='small' 
-                        InputProps={{
-                            endAdornment: <InputAdornment position="start">px</InputAdornment>,
-                        }}/>
-                    <TextField label='Height' fullWidth size='small' 
-                        InputProps={{
-                            endAdornment: <InputAdornment position="start">px</InputAdornment>,
-                        }}/>
-                </Stack>
-                <Grid container alignItems='center' pt={2}>
-                    <Typography>Grid</Typography>
-                </Grid>
-                <Divider/>
-                <Stack direction='column' alignItems='flex-start' spacing={1}>
-                    <FormControlLabel control={<Checkbox />} labelPlacement='start' label="Enabled" />
-                    <TextField label='Width' fullWidth size='small' 
-                        InputProps={{
-                            endAdornment: <InputAdornment position="start">px</InputAdornment>,
-                        }}/>
-                    <TextField label='Height' fullWidth size='small' 
-                        InputProps={{
-                            endAdornment: <InputAdornment position="start">px</InputAdornment>,
-                        }}/>
-                    
-                    <TextField label='Line Size' fullWidth size='small' />
-                    <TextField label='Line Color' fullWidth size='small' />
-                </Stack>
-            </Stack>
-        </Drawer>
-    )  
-
-    return (
-        <Grid item>
-            <IconButton onClick={handleMenuOpen} color='primary' children={<Tune/>}/>
-            {drawer}
+  const drawer = (
+    <Drawer
+      anchor="left"
+      open={isDrawerOpen}
+      onClose={handleMenuClose}
+      PaperProps={{ sx: { mt: 15 } }}
+      BackdropProps={{ invisible: true }}
+    >
+      <Stack
+        spacing={1}
+        width="300px"
+        role="presentation"
+        textAlign="start"
+        p={1}
+      >
+        <Grid container alignItems="center">
+          <IconButton onClick={handleMenuClose} children={<NavigateBefore />} />
+          <Typography>Properties</Typography>
         </Grid>
-    )
-}
+        <Divider />
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <TextField label="Tileset Name" fullWidth size="small" />
+        </Stack>
+        <Grid container alignItems="center" pt={2}>
+          <Typography>Image Size</Typography>
+        </Grid>
+        <Divider />
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <TextField
+            label="Width"
+            fullWidth
+            size="small"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">px</InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            label="Height"
+            fullWidth
+            size="small"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">px</InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
+        <Grid container alignItems="center" pt={2}>
+          <Typography>Grid</Typography>
+        </Grid>
+        <Divider />
+        <Stack direction="column" alignItems="flex-start" spacing={1}>
+          <Stack direction="row" alignItems="flex-start" spacing={1}>
+            <FormControlLabel
+              control={<Checkbox />}
+              labelPlacement="start"
+              label="Enabled"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              labelPlacement="start"
+              label="Restrict To Tile"
+            />
+          </Stack>
+          <TextField
+            label="Width"
+            fullWidth
+            size="small"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">px</InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            label="Height"
+            fullWidth
+            size="small"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">px</InputAdornment>
+              ),
+            }}
+          />
 
-export default TilesetEditorDrawer
+          <TextField label="Line Size" fullWidth size="small" />
+          <TextField label="Line Color" fullWidth size="small" />
+        </Stack>
+      </Stack>
+    </Drawer>
+  );
+
+  return (
+    <Grid item>
+      <IconButton
+        onClick={handleMenuOpen}
+        color="primary"
+        children={<Tune />}
+      />
+      {drawer}
+    </Grid>
+  );
+};
+
+export default TilesetEditorDrawer;
