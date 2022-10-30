8599ce280774da721e6e87974488bdf408bda816
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-10-29 21:26:34 -0400

    Added error messages to community routes. Moved dbm logic to controllers for contest.

--- server/express/routers/TilemapRouter.ts
+++ server/express/routers/TilemapRouter.ts
@@ -1,17 +1,16 @@
 import { Router } from "express";
 import { TilemapController } from "../controllers";
-import { Auth } from "../middleware";
+import { Auth, Upload } from "../middleware";
 
 const router: Router = Router();
 
 router.get("/:id", Auth.verifyJWT, TilemapController.getTilemapById);
 router.put("/:id", Auth.verifyJWT, TilemapController.updateTilemapById);
-router.post("/", Auth.verifyJWT, TilemapController.createTilemap);
+router.post("/", Auth.verifyJWT, Upload.single('image'), TilemapController.createTilemap);
 router.delete("/:id", Auth.verifyJWT, TilemapController.deleteTilemapById);
 
 router.get("/social/:id", TilemapController.getTilemapSocialById);
 router.get("/partials/:search/:sort", Auth.verifyJWT, TilemapController.getTilemapPartials);
-router.put("/:id", Auth.verifyJWT, TilemapController.updateTilemapById);
 router.post("/comment/:id", Auth.verifyJWT, TilemapController.commentTilemapById);
 router.put("/like/:id", Auth.verifyJWT, TilemapController.likeTilemapSocialById);
 router.put("/dislike/:id", Auth.verifyJWT, TilemapController.dislikeTilemapById);
