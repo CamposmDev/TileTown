6439e420e408c123b558a4ea219a4693d0cfa98d
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-11-07 12:22:49 -0500

    Added client to server. Added deploy bash script to hackly build and serve our app.

--- server/express/app.ts
+++ server/express/app.ts
@@ -8,14 +8,12 @@ const app: Express = express();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3000"],
     credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", ApiRouter);
-
-app.use(express.static(path.join(__dirname, '/public')));
 
 export default app;
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,24 @@
 import app from './express/app';
 import { db } from './database/index'
 import dotenv from "dotenv";
+import path from 'path';
+import express from 'express';
 
 // Set up enviorment configuration
 dotenv.config();
-
 const port: string = process.env.PORT || '3000';
 const mongo: string = process.env.MONGO_URI || 'mongodb+srv://Admin:BxXqBUDuPWvof95o@tiletown.bi0xq5u.mongodb.net/?retryWrites=true&w=majority'
 
+// This serves the client application from "localhost:3000" - all the backend routes are at "localhost:3000/api"
+app.use(express.static(path.join(__dirname, 'client')));
+app.use((req, res, next) => {
+    return res.redirect(path.join(__dirname, 'client'));
+});
+
+// Connect to the database and start the express server
 app.listen(port, () => {
     // We'll need to pass in args here to connect to the database.
     db.connect(mongo);
     console.log(`Server started on port ${port}...`);
+    console.log(process.env);
 });
