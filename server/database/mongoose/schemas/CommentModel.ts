ea73c7a367b8ff87ab66edd29b991c5baee49015
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-11-06 19:01:09 -0500

    Started adding default to schema types.

--- server/database/mongoose/schemas/CommentModel.ts
+++ server/database/mongoose/schemas/CommentModel.ts
@@ -7,9 +7,9 @@ const ObjectId = Schema.Types.ObjectId
  * @author Tuyen Vo
  */
 const CommentSchema = new Schema<CommentSchemaType>({
-    author: { type: ObjectId, require: true},
-    body: { type: String, require: true},
+    author: { type: ObjectId, require: true },
     referenceId: { type: ObjectId, require: true},
+    body: { type: String, require: true, default: "Write your comment here!"}
 })
 
 const CommentModel = mongoose.model("CommentModel", CommentSchema);
--- server/database/mongoose/schemas/CommunityModel.ts
+++ server/database/mongoose/schemas/CommunityModel.ts
@@ -9,9 +9,9 @@ const ObjectId = Schema.Types.ObjectId;
 const CommunitySchema = new Schema<CommunitySchemaType>({
     owner: { type: ObjectId, require: true},
     name: { type: String, require: true},
-    description: { type: String, require: true},
-    members: { type: [ObjectId], require: true },
-    visibility: { type: String, require: true}
+    visibility: { type: String, require: true},
+    description: { type: String, require: true, default: "Community description here!"},
+    members: { type: [ObjectId], require: true, default: []}
 });
 
 const CommunityModel = mongoose.model("CommunityModel", CommunitySchema);
