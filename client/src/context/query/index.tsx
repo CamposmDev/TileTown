aed937b605531388ee84d2c8fb20dbf2eeb4fc5a
Michael Campos <camposm.dev@gmail.com>
2022-11-10 01:45:50 -0500

    Created QueryContext
    WIP

---
+++ client/src/context/query/index.tsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react"
+import { useNavigate } from "react-router"
+import { QueryState, QueryStore } from "./QueryStore"
+
+const DefaultQuery = {
+    tilemapResult: [],
+    tilesetResult: [],
+    userResult: [],
+    commResult: [],
+    contestResult: [],
+    forumPostResult: []
+}
+
+const QueryContext = createContext<QueryStore>(new QueryStore({
+    tilemapResult: [],
+    tilesetResult: [],
+    userResult: [],
+    commResult: [],
+    contestResult: [],
+    forumPostResult: []
+}, () => {}, () => {}))
+
+function QueryContextProvider(props: Record<string, any>) {
+    const [query, setQuery] = useState<QueryState>({
+        tilemapResult: [],
+        tilesetResult: [],
+        userResult: [],
+        commResult: [],
+        contestResult: [],
+        forumPostResult: []
+    })
+    const nav = useNavigate()
+    const Query = new QueryStore(query, setQuery, nav)
+    return (
+        <QueryContext.Provider value={Query}>
+            {props.children}
+        </QueryContext.Provider>
+    )
+}
+
+export {QueryContext, QueryContextProvider}
