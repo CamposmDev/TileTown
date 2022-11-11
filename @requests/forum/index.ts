34bf6534627f5c894f4ebacaf8c7754faf5118e8
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-11-11 02:27:55 -0500

    Added requests for forum routes on frontend.

---
+++ @requests/forum/index.ts
@@ -0,0 +1,3 @@
+export type { default as GetForumReq } from './GetForumReq';
+export type { default as DeleteForumReq } from './DeleteForumReq';
+export type { default as CreateForumReq } from './CreateForumReq';
