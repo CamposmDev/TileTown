d94ea86a253edea7f0bb0121265ef0678035aa65
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-11-13 23:46:08 -0500

    Auto deploy test.

--- client/src/components/menu/WelcomeMenu.tsx
+++ client/src/components/menu/WelcomeMenu.tsx
@@ -16,7 +16,7 @@ const WelcomeMenu = () => {
                     noWrap
                     component='div'
                     >
-                        <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>TileTown</Link>
+                        <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>Testing</Link>
                     </Typography>
             </Grid>
             <AccountButton />
