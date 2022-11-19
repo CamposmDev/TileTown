59cf717946bce97b1a5246ddaf3830d283f9fc30
Peter Walsh <peter.t.walsh@stonybrook.edu>
2022-11-19 18:15:33 -0500

    Comment controller and router for getting comments by id.

---
+++ server/express/controllers/CommentController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { db } from "../../database";
+
+
+export default class CommentController {
+
+    public static async getCommentById(req: Request, res: Response): Promise<Response> {
+        if (!req || !req.params) {
+            return res.status(400).json({ message: "Bad Request" });
+        }
+        if(!req.params.id) {
+            return res.status(400).json({ message: "Missing comment id" });
+        }
+
+        let comment = await db.comments.getCommentById(req.params.id);
+        if (comment === null) { 
+            return res.status(404).json({ message: "Comment not found" });
+        }
+
+        return res.status(200).json({ message: "Comment found!" });
+    }
+}
