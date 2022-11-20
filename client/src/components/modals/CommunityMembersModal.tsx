65139cf71f5924790b3b1bb8b4215a494a4612d8
Tuyen Vo <thituyen1996@gmail.com>
2022-11-20 16:02:41 -0500

    Updated CommunityMemberModal
    Displays members in community

--- client/src/components/modals/CommunityMembersModal.tsx
+++ client/src/components/modals/CommunityMembersModal.tsx
@@ -4,54 +4,38 @@ import Box from '@mui/material/Box';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import Typography from '@mui/material/Typography';
 import { Dialog, DialogContent, Drawer, Grid, Icon, IconButton, Modal } from '@mui/material';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Group } from '@mui/icons-material';
 import UserProfileCard from '../card/UserProfileCard';
 import { SLIDE_DOWN_TRANSITION } from '../util/Constants';
+import { CommunityContext } from 'src/context/social/community';
 
 
 
 const CommunityMembersModal = () => {
+    const comm = useContext(CommunityContext)
     const [isOpen, setIsOpen] = useState(false)
     const handleClose = () => setIsOpen(false);
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 500,
-        bgcolor: 'background.paper',
-        boxShadow: 1,
-        p: 4,
-        borderRadius: 2
-
-      };
-
+    let c = comm.getCurrentCommunity()
+    let members: JSX.Element | JSX.Element[] = <div/>
+    if (c) {
+        members = c.members.map(x =>
+            <Grid container item>
+                <UserProfileCard userId={x} />
+            </Grid>
+        )
+    }
     let ui = (
         <Drawer 
             open={isOpen} 
             onClose={handleClose}
             anchor='right'
-            >
+        >
             <DialogContent>
             <Box>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    RPGs Done Right Members
-                </Typography>
-                <Avatar sx={{ bgcolor: 'secondary.main', justifyItems: 'center', margin: 'auto'}}>
-                    <Icon ><PeopleAltIcon /></Icon>
-                </Avatar>
+                <Typography variant="h6">Members</Typography>
                 <Grid sx={{height: '500px', overflow: 'auto', mt: 1}} spacing={1}>
-                {/* {[1,1,1,1,,1,1,1,1,1,1,1].map((x,i) => 
-                    <Grid item mt={1}>
-                        <UserProfileCard
-                        firstName='Michael'
-                        lastName="Campos"
-                        username="Camposm"
-                    />
-                    </Grid>
-                )} */}
+                    {members}
                 </Grid>        
             </Box>
             </DialogContent>
