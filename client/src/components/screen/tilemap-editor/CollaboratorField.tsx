17fee914f24c5db0834078dc1c220cce3cbe3c3d
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-12-02 02:34:40 -0500

    layer/collab/tileset drawer now checks for current collaborator and owner

--- client/src/components/screen/tilemap-editor/CollaboratorField.tsx
+++ client/src/components/screen/tilemap-editor/CollaboratorField.tsx
@@ -3,6 +3,7 @@ import { TilemapEditContext } from "src/context/tilemapEditor";
 import { SnackContext } from "src/context/snack";
 import { Delete } from "@mui/icons-material";
 import { useContext } from "react";
+import { AuthContext } from "src/context/auth";
 
 interface CollabProps {
   index: number;
@@ -11,14 +12,19 @@ interface CollabProps {
 }
 
 const CollaboratorField = (props: CollabProps) => {
+  const auth = useContext(AuthContext);
   const edit = useContext(TilemapEditContext);
   const snack = useContext(SnackContext);
+
+  const user = auth ? auth.usr : undefined;
+  const id = user ? user.id : undefined;
   return (
     <Stack pl={1} pr={1} direction="row" alignItems="center">
       <Typography>{props.name}</Typography>
       <Tooltip title="Remove Collaborator" arrow>
         <IconButton
           color="error"
+          disabled={!edit.canEdit(id) || !edit.isOwner(id)}
           onClick={() => {
             snack.showSuccessMessage(
               `User ${props.name} was removed as collaborator`
